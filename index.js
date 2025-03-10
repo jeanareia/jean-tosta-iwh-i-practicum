1bd5b493fdf88c5240933290d0a46a4b96100622
jeantareia <jean.tareia@gmail.com>
2025-03-10 16:12:22 -0300

    Fixed app.get'update-cobj' route

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PRIVATE_APP_ACCESS = 'process.env.PRIVATE_APP_ACCESS';
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-app.get('/updat-cobj', async(req,res)=>{
+app.get('/update-cobj', async(req,res)=>{
     try{
         res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
     }
@@ -32,6 +32,10 @@ app.get('/updat-cobj', async(req,res)=>{
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req,res)=>{
+    console.log("--> " + req.body);
+
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
